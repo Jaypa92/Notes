1eca73fdd3d48ff00e9f60bdb55beaf280d406a3
[Jaypa92] <justinparris.a@gmail.com>
2025-03-24 20:23:52 -0400

    postgresql node connection

--- Databases/Postgresql/Node/server.js
+++ Databases/Postgresql/Node/server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const pool = require("./db");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT NOW()");
+        res.json({ success: true, timestamp: result.rows[0]});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Database Connection Error");
+    }
+});
+
+app.listen(5000, () => console.log("Server Running on Port 5000"));
