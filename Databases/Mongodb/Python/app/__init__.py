600306d555bac3ef35885133197004dd1296c8d7
[Jaypa92] <justinparris.a@gmail.com>
2025-03-28 19:04:14 -0400

    commit

--- Databases/Mongodb/Python/app/__init__.py
+++ Databases/Mongodb/Python/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from Python.config import Config
+from config import Config
 
 mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    from Python.routes import main
+    from routes import main
     app.register_blueprint(main)
 
     return app
