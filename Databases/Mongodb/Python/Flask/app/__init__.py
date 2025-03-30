4527c747036e781b7830a2a12da61202b1cebdb8
[Jaypa92] <justinparris.a@gmail.com>
2025-03-30 13:24:08 -0400

    commit

--- Databases/Mongodb/Python/Flask/app/__init__.py
+++ Databases/Mongodb/Python/Flask/app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from config import Config
 
-mongo = PyMongo()
+mongo = PyMongo() # Initializes the PyMongo object to handle MongoDB operations
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+    app = Flask(__name__) # Creates instance of the Flask app. __name__ tells Flask where to find the loaction of the application
+    app.config.from_object(Config) # Loads the configuration settings from the Config class
     from routes import main
-    app.register_blueprint(main)
+    app.register_blueprint(main) # The main bluprint is imported to allowing for modular routes and views
 
     return app
